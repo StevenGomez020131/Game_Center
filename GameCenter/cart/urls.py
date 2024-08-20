8e3dfa31a3455147a2e889698f8be36bb26ee578
EmilianoIbarraMX <augusstop@gmail.com>
2024-08-19 23:25:32 -0600

    CarritoAgregarJuego

--- GameCenter/cart/urls.py
+++ GameCenter/cart/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 
+
+
 urlpatterns = [
     path('',views.cart_summary, name="cart_summary"),
     path('add/',views.cart_add, name="cart_add"),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('update/',views.cart_update, name="cart_update"),
 
 ]
+
+
+
--- GameCenter/cart/views.py
+++ GameCenter/cart/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from productos.models import accesoriosXbox
+from productos.models import accesoriosPlayStation
+from productos.models import accesoriosNintendo
+from productos.models import accesoriosPC
+from productos.models import JuegosXbox
+from django.http import JsonResponse
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    #Obtener el carrito
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    return render(request, "cart_summary.html", {"cart_products":cart_products})
 
 def cart_add(request):
-    pass
+    #Obtener el carrito
+    cart = Cart(request)
+
+    #Probamos para el POST
+    if request.POST.get('action') == 'post':
+
+        #Obtener las cosas
+        product_id =int(request.POST.get('product_id'))
+
+        #Buscar el producto/accesorio en la DB
+        product =get_object_or_404(JuegosXbox, id=product_id)
+
+        #Para guardar la sesion
+        cart.add(product=product)
+
+        #Obtener la cantidad del carrito
+        cart_quantity = cart.__len__()
+
+        #Devolver la repuesta
+        #response = JsonResponse({'Product Name: ': product.nombre})
+        response = JsonResponse({'qty: ': cart_quantity})
+        return response
+
 
 def cart_delete(request):
     pass
