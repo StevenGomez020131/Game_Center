a9f810dcb7614667a898e1467a6a65bb6b3eaeb5
“StevenGomez020131” <colombianodavid2002@mail.com>
2024-08-21 22:18:29 -0600

    avance

--- GameCenter/comentarios/admin.py
+++ GameCenter/comentarios/admin.py
@@ -3,13 +3,7 @@ from .models import comentarios
 from .models import ComentarioContacto
 # Register your models here.
 
-class AdministrarComentarios(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
-    list_display = ('nombre', 'created')
-    search_fields = ('nombre', 'created', 'updated')
-    date_hierarchy = 'created'
-  
-admin.site.register(comentarios, AdministrarComentarios)
+
 
 class AdministrarComentariosContacto(admin.ModelAdmin):
     list_display = ('id', 'mensaje')
